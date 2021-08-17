bf9f42406b804c94921e5e131ede26b66bb24f19
Benjamin Goddard <bdgcypher@gmail.com>
2021-08-16 21:48:02 -0600

    added email functtionality.

--- src/components/MessageMe/index.js
+++ src/components/MessageMe/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import emailjs from 'emailjs-com';
 import {
     Container,
     FormWrap,
@@ -12,20 +13,33 @@ import {
 } from './MessageMeElements';
 
 const MessageMe = () => {
+
+    function sendEmail(e) {
+        e.preventDefault();
+
+        emailjs.sendForm('service_bisd5ko', 'template_6av43zj', e.target, 'user_gnPTqdY8hKecqwUQdpmhB')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+        e.target.reset()
+    }
+
     return (
         <>
             <Container>
                 <FormWrap>
                     <Icon to="/" id="top">BG</Icon>
                     <FormContent>
-                        <Form action="#">
+                        <Form onSubmit={sendEmail}>
                             <FormH1>Send me a message</FormH1>
                             <FormLabel htmlFor="for">Name</FormLabel>
-                            <FormInput type="text" required />
+                            <FormInput name="name" type="text" required />
                             <FormLabel htmlFor="for">Email</FormLabel>
-                            <FormInput type="email" required />
+                            <FormInput name="email" type="email" required />
                             <FormLabel htmlFor="for">Message</FormLabel>
-                            <FormInput type="text" required />
+                            <FormInput name="message" type="text" required />
                             <FormButton type="submit">Send</FormButton>
                         </Form>
                     </FormContent>
